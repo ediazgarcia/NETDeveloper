26e79a124f63a3b7a19869eec7d38d9d1e6d6ccc
ediazgarcia <eddymanueldiazgarcia@gmail.com>
2024-03-11 15:24:37 -0400

    Tipos Anonimos y Tuplas

---
+++ TiposAnonimos/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace TiposAnonimos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new
+            {
+                Name = "Eddy",
+                Age = 28,
+                fecha = DateTime.Now
+
+            };
+
+            Console.WriteLine($"{person.Name} , {person.Age}");
+
+
+            var person1 = new[]
+            {
+               new{ Name = "Eddy", Age = 28, fecha = DateTime.Now },
+             
+            };
+
+            foreach (var p in person1)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
---
+++ Tuplas/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Tuplas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            (int id, string name) product = (1, "Cerveza Republica");
+
+            Console.WriteLine($"{product.id}, {product.name}");
+
+            product.name = "Cerveza Presidente";
+            Console.WriteLine($"{product.id}, {product.name}");
+
+            var person = (1, "Eddy");
+            Console.WriteLine(person);
+
+            var cityInfo = getLocationRD();
+            Console.WriteLine($"lat: {cityInfo.lat}, lng: {cityInfo.lng}, name: {cityInfo.name}");
+        }
+
+        public static (float lat, float lng, string name) getLocationRD()
+        {
+            float lat = 18.65656323f;
+            float lng = 99.635656565f;
+            string name = "RD";
+
+            return (lat, lng, name);
+        }
+    }
+}
