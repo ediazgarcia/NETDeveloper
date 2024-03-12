cd53bb3e3fd929af7c42556f862b7bb3f20b8241
ediazgarcia <eddymanueldiazgarcia@gmail.com>
2024-03-12 08:06:24 -0400

    Excepciones

---
+++ Excepciones/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+
+
+
+namespace Excepciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            try
+            {
+            string content = File.ReadAllText(@"C:\\Users\\eddym\\Desktop\\workspace\\args.txt");
+            Console.WriteLine(content);
+
+               throw new Exception("Ocurrio algo");
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine("El archivo no existe");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Se ejecuta pase lo que pase en la app");
+            }
+            Console.WriteLine("Aqui se sigue ejecutando");
+
+
+        }
+    }
+}
